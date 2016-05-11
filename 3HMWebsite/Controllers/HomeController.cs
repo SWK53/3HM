547d8a034884f8e91da8ba7b1fc66798532b4a0e
Shabbir Salumberwala <ssalumberwala@psc.com.kw>
2016-05-11 09:00:11 +0300

    Initial Change

---
+++ 3HMWebsite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3HMWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string language)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            return View();
+        }
+    }
+}
